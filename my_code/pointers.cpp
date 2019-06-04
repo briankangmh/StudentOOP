a9c9ea0abacb0bbe7f85756a5bf4edda593be4b9
Brian Kang <briankang@icloud.com>
2019-06-04 12:46:23 -0400

    pointers
    sdf

--- my_code/pointers.cpp
+++ my_code/pointers.cpp
@@ -12,33 +12,54 @@
 using namespace std;
 
 void inc_by_value(int n) {
+    n++;
 }
 
 
 void inc_with_pointer(int* iptr) {
+//    (*iptr)+1;
+    *iptr +=1;
 }
 
 
 void inc_with_reference(int& n) {
+    n++;
 }
 
 
 Thing** create_array_of_things(int n) {
-    return nullptr;
+    Thing** array = new Thing*[n];
+    for (int i = 0; i < n; i++){
+        array[i] = new Thing(i);
+    }
+    return array;
 }
 
-void print_all_things(Thing** things, int n) {
+void print_all_things(Thing** things, int n){
+    for (int i = 0; i < n; i++){
+        cout << things[i] -> val << " ";
+    }
+    cout <<endl;
 }
 
-void double_all_things(Thing** things, int n) {
+void double_all_things(Thing** things, int n){
+    for (int i = 0; i < n; i++){
+        things[i]->val*=2;
+    }
 }
 
 void delete_all_things(Thing** things, int n) {
+    for (int i = 0; i < n; i++){
+        delete things[i];
+        things[i] = nullptr;
+    }
+    delete[] things;
+    things = nullptr;
 }
 
 
-void assignTA(Student& s, Student& ta) {
-}
-
-void printTAs(vector<Student> students) {
-}
+//void assignTA(Student& s, Student& ta) {
+//}
+//
+//void printTAs(vector<Student> students) {
+//}
