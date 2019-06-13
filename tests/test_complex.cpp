6363d8c2aa46f1321610985a3fad3fa89541b0ac
Brian Kang <briankang@icloud.com>
2019-06-13 12:33:44 -0400

    final
    yyay

--- tests/test_complex.cpp
+++ tests/test_complex.cpp
@@ -11,16 +11,16 @@ int main() {
     cout << "Input a complex number:\n";
     Complex c1{12, 13};
     // get a complex from stdin:
-        cin >> c1;
-        if(!cin)
-        {
-            cerr << "Bad input format\n";
-            exit(1);
-        }
-        if(deeznuts)
-        {
-            cout << "c1 = " << c1 << endl;
-        }
+//        cin >> c1;
+//        if(!cin)
+//        {
+//            cerr << "Bad input format\n";
+//            exit(1);
+//        }
+//        if(deeznuts)
+//        {
+//            cout << "c1 = " << c1 << endl;
+//        }
     
         Complex c2{43.2, 58.9};
         assert(c2.get_real() == 43.2);
